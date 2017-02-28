5d15053d5ffed988bd19712cb4884d73c0ee78b2
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-02-28 14:40:20 +0100

    INIT

--- form.controller.js
+++ form.controller.js
@@ -2,10 +2,5 @@ angular.module('app').controller('FormCtrl', function ($scope) {
 
 	$scope.listaUtenti = [];
 
-	$scope.inserisci = function (user, form) {
-		$scope.listaUtenti.push(user);
-		$scope.user = {};
-		$scope.myform.$setPristine();
-	}
 
 });
